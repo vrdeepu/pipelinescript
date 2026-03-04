cad62fac28393ac9c4cb15f17d6cfd96ebebe8b7
vrdeepu <vr.deepu@yahoo.com>
2026-03-05 00:36:06 +0530

    adding script code to repo

--- script.groovy
+++ script.groovy
@@ -10,25 +10,42 @@ pipeline {
     stages {
         stage('CI: Build App') {
             steps {
-                // We create a specific folder for the App
                 dir('app-code') {
                     git branch: 'main', url:'https://github.com/vrdeepu/helloworld-gcp.git'
-                    // Now Maven runs INSIDE the 'app-code' folder where the pom.xml is
-                    bat 'dir /s /b pom.xml'
                     bat 'mvn clean package'
                 }
-            
-                
             }
         }
 
-        stage('CD: Infrastructure Plan') {
+        stage('CD: Infra Apply') {
             steps {
-                // We create a specific folder for the Infra
                 dir('infra-code') {
-                   git branch: 'main', url:'https://github.com/vrdeepu/helloworld-infra.git'
+                    git branch: 'main', url:'https://github.com/vrdeepu/helloworld-infra.git'
+                    echo "Initializing and Provisioning..."
                     bat 'terraform init'
-                    bat 'terraform plan'
+                    bat 'terraform apply -auto-approve'
+                }
+            }
+        }
+
+        stage('CD: Deploy JAR to VM') {
+            steps {
+                // We run this from the root workspace so it can see both 'app-code' and 'infra-code'
+                bat """
+                    gcloud compute scp app-code/target/helloworld-gcp-1.0-SNAPSHOT.jar helloworld-vm:/tmp/app.jar --zone=us-central1-a --quiet
+                    gcloud compute ssh helloworld-vm --zone=us-central1-a --quiet --command="java -jar /tmp/app.jar &"
+                """
+            }
+        }
+
+        stage('Cleanup: Manual Approval') {
+            steps {
+                script {
+                    input message: "App is deployed! Check the IP, then click proceed to DESTROY.", ok: "Destroy Now"
+                }
+                dir('infra-code') {
+                    echo "Cleaning up Infrastructure..."
+                    bat 'terraform destroy -auto-approve'
                 }
             }
         }
